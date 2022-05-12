971ff5cbf23fb1c0be97ba344f79a2caf320ca36
Grimos10 <biagiogrimos@gmail.com>
2022-05-12 15:09:53 +0200

    jamm meee

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="navbar" class="flexbox justify-between align-center">
-        <section><h1 class="font-size-2em" style="color: #ffffff">METALLICART SRLS</h1></section>
+        <img src="@/assets/img/pogle.jpeg">
         <section>
             <a href="/" class="font-size-1-25em font-weight-lighter">Home</a>
-            <a href="/#contatti" class="font-size-1-25em font-weight-lighter">Contatti</a>
             <a href="/#chi-siamo" class="font-size-1-25em font-weight-lighter">Chi Siamo</a>
             <a href="/#servizi" class="font-size-1-25em font-weight-lighter">Servizi</a>
+            <a href="/#contatti" class="font-size-1-25em font-weight-lighter">Contatti</a>
         </section>
     </div>
     <slot></slot>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,30 @@
 <template>
     <div>
         <section id="main" class="container">
-            <div class="content"></div>
-            <h1 class="font-size-8em" style="color: #ffffff">METALLIC ART SRLS</h1>
-        </section>
-        <section id="contatti" class="container">
-            <div class="content"></div>
+            <div class="content flexbox">
+                <div class="flexbox align-center">
+                    <h1 class="font-size-7em color-white half">METALLIC ART SRLS</h1>
+                </div>
+                <div class="flexbox align-center center half">
+                    <div>
+                        <p class="color-white font-size-1-5em">
+                            Metallic Art srls è una nuova azienda in grado di soddisfare
+                            le più moderne esigenze nel campo della carpenteria metallica 
+                            di precisione.
+                        </p>
+                        <button>PORCO DIO</button>
+                        
+                    </div>
+                </div>
+            </div>
         </section>
         <section id="chi-siamo" class="container">
-            
+            <div class="content"></div>
         </section>
         <section id="servizi" class="container">
+            
+        </section>
+        <section id="contatti" class="container">
             
         </section>
     </div>
@@ -20,7 +34,7 @@
         background: url(@/assets/img/bg2.jpg) no-repeat center center scroll;
         background-size: cover;
     }
-    section#contatti{
+    section#chi-siamo{
         background: url(@/assets/img/bg3.jpg) no-repeat center center scroll;
         background-size: cover;
     }
